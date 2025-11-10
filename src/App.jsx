744e0d2e30c33577eeab99482fc42bfb652a4537
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-11-10 12:48:47 +0100

    added array of articles and made it dynamic on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 function App() {
 
+  const articles = [
+    {
+      id: 1,
+      title: "Introduzione a React"
+    },
+    {
+      id: 2,
+      title: "Come funziona il Virtual DOM"
+    },
+    {
+      id: 3,
+      title: "Guida a useState e useEffect"
+    },
+    {
+      id: 4,
+      title: "Ottimizzare le performance in React"
+    },
+    {
+      id: 5,
+      title: "React Router: creare una SPA completa"
+    }
+  ];
+
 
   return (
     <>
+      <div className="container">
+        <div className="col-6 mx-auto">
+          <h1 className="display-6">Ultimi articoli su React</h1>
+          <div className="card">
+            <ul class="list-group">
+              {
+                articles.map(item =>
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {item.title}
+                    <button className="btn btn-success">Read</button>
+                  </li>
+                )
 
+              }
+
+            </ul>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
